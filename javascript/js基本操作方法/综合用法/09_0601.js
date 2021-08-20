d8694a2a436aed71d3a8871733a68b25e038fc09
yuyuanjun <yuyuanjun_123@163.com>
2021-08-21 01:00:26 +0800

    fix

--- javascript/js基本操作方法/综合用法/09_0601.js
+++
@@ -1,9 +0,0 @@
-var str = "you can you up no can no bb"
-console.log()
-var arr = str.split(" ")
-var strAll = ""
-for (let i = 0; i < arr.length; i++) {
-    strAll = strAll + " " + arr[i].slice(0, 1).toLocaleUpperCase() + arr[i].slice(1)
-
-}
-console.log(strAll)
