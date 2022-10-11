92369599527b6cb2ca169acf0136c823f5c3efa4
yuyuanjun <yuyuanjun_123@163.com>
2022-10-11 17:08:17 +0800

    x

---
+++ c/02 变量和表达式/01 从外部输入值创建变量/change.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// #define _CRT_SECURE_NO_WARNINGS
+int main()
+{
+    int price = 0;
+    printf("«Î ‰»ÎΩ∂Ó£∫");
+    scanf("%d", &price);
+    int change = 100 - price;
+    printf("%d\n", change);
+    return 0;
+
+}
+
