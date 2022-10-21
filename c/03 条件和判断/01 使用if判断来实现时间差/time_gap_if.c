296741b925985e1c24e815408640985dc96d6e4d
yuyuanjun <yuyuanjun_123@163.com>
2022-10-21 20:43:27 +0800

    x

--- c/03 条件和判断/01 使用if判断来实现时间差/time_gap_if.c
+++ c/03 条件和判断/01 使用if判断来实现时间差/time_gap_if.c
@@ -1,4 +1,3 @@
-// 输入两个时间，并计算时间差
 #include <stdio.h>
 int main()
 {
@@ -8,9 +7,9 @@ int main()
     scanf("%d %d",&hour1,&minute1);
     printf("please type the second time: ");
     scanf("%d %d",&hour2,&minute2);
-    int hour3 = (hour1 * 60 + minute1 - hour2 * 60 - minute2)/60;
-    int minute3 = (hour1 * 60 + minute1 - hour2 * 60 - minute2)%60;
-    printf("the time gap is %dh%dm",hour3,minute3);
-    return 0;
-
+    if(minute1 >= minute2){
+        printf("%dh%dm",hour1-hour2,minute1-minute2);
+    }else{
+        printf("%dh%dm",hour1-1-hour2,minute1+60-minute2); 
+    }
 }
---
+++ c/03 条件和判断/02 比较运算符得出的结果是1或0/compare.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("%d\n",5>3==6>4);
+    printf("%d\n",6>5>4);
+}
