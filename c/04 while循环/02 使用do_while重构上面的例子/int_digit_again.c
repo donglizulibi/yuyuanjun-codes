32d4ff3f3fc78d5f941cd19d34ab5c60e70a43f2
yuyuanjun <yuyuanjun_123@163.com>
2022-10-25 22:17:45 +0800

    x

---
+++ c/04 while循环/02 使用do_while重构上面的例子/int_digit_again.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int count = 0;
+    int number = 0;
+    printf("please type the number: ");
+    scanf("%d",&number);
+    int mid = number/10;
+
+    if( mid > 0 ){
+        count++;
+    }
+
+    do{
+        mid /= 10;
+        count++;
+    } while(mid > 0);
+
+    printf("the digit of this number is %d",count);
+
+    
+}
