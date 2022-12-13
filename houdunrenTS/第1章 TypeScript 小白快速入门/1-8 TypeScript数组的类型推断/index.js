a5907f7b877a940557e6660099d8472ef6e68043
yuyuanjun <yuyuanjun_123@163.com>
2022-12-13 22:13:16 +0800

    x

---
+++ houdunrenTS/第1章 TypeScript 小白快速入门/1-8 TypeScript数组的类型推断/index.js
@@ -0,0 +1,9 @@
+var hd = ['houdunren'];
+// const hd: string[] 表示对一个数组进行类型限制，数组里面的项目只能是字符串
+hd.push('hdcms');
+// hd.push(123)
+console.log(hd);
+var arr = ['houdunwang', 123, false];
+// const arr: (string | number | boolean)[]
+arr.push('str', 222, true);
+console.log(arr);
