d88224f050680a5c1a510dc8b25778a1b67a611b
yuyuanjun <yuyuanjun_123@163.com>
2023-02-25 17:23:55 +0800

    x

---
+++ xuchao-fifth/08 vue3.0（重构版本）/00 Vue汇总/vue-composite/src/components/vueThree/bVueComponent/eVmodelComponent/AllVmodelComponent/aAchieveVmodel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h5>input和value方法</h5>
+    <h5>input标签内的input绑定中传入$event实参</h5>
+    <h5>绑定value值是为了使初始值相同</h5>
+    <input type="text" :value="content" v-on:input="inputValue($event)" />
+    <!-- <input type="text" :value="content" @input="content = $event.target.value" /> -->
+    <span>{{ content }}</span>
+    <br />
+    <br />
+    <h5>v-model方法</h5>
+    <input type="text" v-model="title" />
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: "hdcms",
+      title: "hdcms",
+    };
+  },
+  methods: {
+    inputValue(event) {
+      this.content = event.target.value;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
+·
--- xuchao-fifth/08 vue3.0（重构版本）/00 Vue汇总/vue-composite/src/components/vueThree/bVueComponent/eVmodelComponent/eVmodelComponent.vue
+++ xuchao-fifth/08 vue3.0（重构版本）/00 Vue汇总/vue-composite/src/components/vueThree/bVueComponent/eVmodelComponent/eVmodelComponent.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>asdf</div>
+  <div>
+    <h4>v-model的同效实现方法</h4>
+    <a-achieve-vmodel></a-achieve-vmodel>
+  </div>
 </template>
 
 <script>
-export default {};
+import aAchieveVmodel from "./AllVmodelComponent/aAchieveVmodel.vue";
+export default {
+  components: {
+    aAchieveVmodel,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
