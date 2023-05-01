cf13613b826a2b2e08a0303136b9313475424ecb
yuyuanjun <yuyuanjun_123@163.com>
2023-05-01 23:51:32 +0800

    x

--- houdunrenTS/第1章 TypeScript 小白快速入门/1-16 unknown与any的区别及as类型断言/01 unknown与any的区别/index.js
+++ houdunrenTS/第1章 TypeScript 小白快速入门/1-16 unknown与any的区别及as类型断言/01 unknown与any的区别/index.js
@@ -1,24 +1,27 @@
-// 现在分别使用any和unknow类型定义变量
-// 都是先把各自的变量赋值为一个字符串
-// 然后再把这个变量赋值给一个string变量
-// 结果使用any类型的变量可以赋值成功
-// 而使用unknow类型的变量无法赋值
-var hd = 'houdunren';
+// 之前有例子讲到，如果在创建一个类的实例时，给这个实例加上any类型
+// 则当调用实例中的并不存在的方法时，并不会报错，因为any不会进行ts校验
+// 现在定义一个any类型的变量，不给这个变量赋值，则变量的类型是undefined
+// 将一个数值赋值给这个变量，此时检测这个变量的类型，为number
+// 然后再定义一个变量，将这个变量的类型定义为string，变量的值由上个变量所赋予
+// 后定义的变量的类型也成了number
+// let hd:any
+var hd = 123;
+console.log(typeof hd);
 var a = hd;
 console.log(a);
 console.log(typeof a);
-var xj = 'hdcms';
-// let b:string= xj
-// 这两种赋值都可以
-// let b= xj
-// let b:unknown= xj
-// 虽然xj变量的类型还是string，但是还是不能赋值给unknow类型变量
-console.log(typeof xj);
-// 结论就是any类型可能是string、number、Boolean等之中的某个类型
-// 而unknown不属于任何类型，不是string，也不是number
-// 即使xj变量的值就是一个字符串，但是这个变量的类型定义为unknow类型
-// 就不会是string
-// 就相当于
-// let hc = 123
-// let c:string = hc
-// hc已经是number类型，就不会再是string类型
+// 现在再定义一个变量，设置为unknown类型，并以数值赋值给这个变量
+// 类型检测到这个变量类型是number
+// 再创建一个变量，设置为string，将上面的unknown变量赋值给这个变量
+// 是无法赋值的
+// 但是如果创建的变量是any或者unknown本身，则可以把这个原本的unknown类型赋值上去
+var hdd = 123;
+console.log(typeof hdd);
+// let b:string = hdd
+var c = hdd;
+var d = hdd;
+// any和unknown都是ts中的顶级类型，二者都可以被赋值于任意类型的数据
+// 
+var obj = {};
+obj.name = 'houdunren';
+console.log(obj);
--- houdunrenTS/第1章 TypeScript 小白快速入门/1-16 unknown与any的区别及as类型断言/01 unknown与any的区别/index.ts
+++ houdunrenTS/第1章 TypeScript 小白快速入门/1-16 unknown与any的区别及as类型断言/01 unknown与any的区别/index.ts
@@ -1,2 +1,34 @@
 // 之前有例子讲到，如果在创建一个类的实例时，给这个实例加上any类型
-// 则
+// 则当调用实例中的并不存在的方法时，并不会报错，因为any不会进行ts校验
+
+// 现在定义一个any类型的变量，不给这个变量赋值，则变量的类型是undefined
+// 将一个数值赋值给这个变量，此时检测这个变量的类型，为number
+
+// 然后再定义一个变量，将这个变量的类型定义为string，变量的值由上个变量所赋予
+// 后定义的变量的类型也成了number
+// let hd:any
+let hd:any = 123
+console.log(typeof hd)
+let a:string = hd
+console.log(a)
+console.log(typeof a)
+
+// 现在再定义一个变量，设置为unknown类型，并以数值赋值给这个变量
+// 类型检测到这个变量类型是number
+// 再创建一个变量，设置为string，将上面的unknown变量赋值给这个变量
+// 是无法赋值的
+// 但是如果创建的变量是any或者unknown本身，则可以把这个原本的unknown类型赋值上去
+let hdd:unknown = 123
+console.log(typeof hdd)
+// let b:string = hdd
+let c:any = hdd
+let d:unknown = hdd
+
+// any和unknown都是ts中的顶级类型，二者都可以被赋值于任意类型的数据
+// 
+
+let obj:any = {}
+obj.name = 'houdunren'
+console.log(obj)
+
+
