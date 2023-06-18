e93b548004d40f08d9624678d0c1643d37b65757
yuyuanjun <yuyuanjun_123@163.com>
2023-06-18 20:14:21 +0800

    x

---
+++ xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-component-library/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Pagination from "./components/Pagination/Pagination.vue";
+import ProvCityZoneComponent from "./components/ProvCityZoneComponent/ProvCityZoneComponent.vue";
+import { ref } from "vue";
+const currentPage = ref(1);
+const total = ref(10);
+</script>
+
+<template>
+  <Pagination :totalPagesData="total" v-model="currentPage" />
+  <ProvCityZoneComponent></ProvCityZoneComponent>
+</template>
+
+<style scoped></style>
