e15f6101c71d55b50ac93cbd56613cf61e635cc7
yuyuanjun <yuyuanjun_123@163.com>
2023-06-20 22:21:09 +0800

    x

--- xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-component-library/src/App.vue
+++ xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-component-library/src/App.vue
@@ -1,14 +1,14 @@
-<script setup>
-import Pagination from "./components/Pagination/Pagination.vue";
-import ProvCityZoneComponent from "./components/ProvCityZoneComponent/ProvCityZoneComponent.vue";
-import { ref } from "vue";
-const currentPage = ref(1);
-const total = ref(10);
+<script setup lang="ts">
+// import Pagination from "./components/Pagination/Pagination.vue";
+// import ProvCityZoneComponent from "./components/ProvCityZoneComponent/ProvCityZoneComponent.vue";
+// import { ref } from "vue";
+// const currentPage = ref(1);
+// const total = ref(10);
 </script>
 
 <template>
-  <Pagination :totalPagesData="total" v-model="currentPage" />
-  <ProvCityZoneComponent></ProvCityZoneComponent>
+  <!-- <Pagination :totalPagesData="total" v-model="currentPage" />
+  <ProvCityZoneComponent></ProvCityZoneComponent> -->
 </template>
 
 <style scoped></style>
