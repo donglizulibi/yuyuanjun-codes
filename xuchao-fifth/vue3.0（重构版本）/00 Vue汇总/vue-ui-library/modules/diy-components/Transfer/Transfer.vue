31fc380719ead45ad89869f6b7e407e9da69a0d4
yuyuanjun <yuyuanjun_123@163.com>
2023-06-25 23:41:38 +0800

    x

--- xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-ui-library/modules/diy-components/Transfer/Transfer.vue
+++ xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-ui-library/modules/diy-components/Transfer/Transfer.vue
@@ -7,25 +7,190 @@
         </option>
       </select>
     </div>
+
     <div class="transfer">
       <div class="box left-list">
-        <h1>{{ leftTitle }}</h1>
+        <h1 class="list-title">
+          <input
+            id="chooseAllLeft"
+            type="checkbox"
+            :disabled="chooseDisabledLeft()"
+            @click="chooseAllLeft($event.target.checked, leftListData, checkedData)"
+          />{{ leftTitle }}
+        </h1>
+        <div>
+          <div
+            v-for="item of leftListData"
+            :key="item.id"
+            :class="['list-item', item.disabled ? 'disabled' : '']"
+          >
+            <input
+              type="checkbox"
+              :id="'checkbox__' + item.id"
+              :disabled="item.disabled"
+              @click="setCheckedData($event.target.checked, 'left', item)"
+            />
+
+            <label :for="'checkbox__' + item.id">
+              {{ item.phone_name }}
+            </label>
+          </div>
+        </div>
       </div>
-      <div class="box button-group"></div>
-      <div class="box right-list"></div>
+      <div class="box button-group">
+        <button
+          :disabled="transferDisabled.left"
+          @click="removeRightListData(checkedData.right)"
+        >
+          &lt;
+        </button>
+        <button
+          :disabled="transferDisabled.right"
+          @click="addRightListData(checkedData.left)"
+        >
+          &gt;
+        </button>
+      </div>
+      <div class="box right-list">
+        <h1 class="list-title">
+          <input
+            id="chooseAllRight"
+            type="checkbox"
+            :disabled="rightListData.length == 0"
+            @click="chooseAllRight($event.target.checked, rightListData, checkedData)"
+          />{{ rightTitle }}
+        </h1>
+        <div>
+          <div
+            v-for="item of rightListData"
+            :key="item.id"
+            :class="['list-item', item.disabled ? 'disabled' : '']"
+          >
+            <input
+              type="checkbox"
+              :id="'checkbox__' + item.id"
+              :disabled="item.disabled"
+              @click="setCheckedData($event.target.checked, 'right', item)"
+            />
+
+            <label :for="'checkbox__' + item.id">
+              {{ item.phone_name }}
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import propsDefination from "./extends/props";
-import { useTargetIndex, useComputedData } from "./extends/hooks";
+import {
+  useRightListData,
+  useTargetIndex,
+  useComputedData,
+  useCheckedData,
+  useChooseAll,
+} from "./extends/hooks";
 
 const props = defineProps(propsDefination);
 const options = props.data.map((item) => item.title);
 
 const [targetIndex, setTargetIndex] = useTargetIndex(0);
-const { leftTitle } = useComputedData(props.data, targetIndex);
+
+const [removeCheckedData, addCheckedData, checkedData] = useCheckedData();
+
+const setCheckedData = (checked, leftOrRight, item) => {
+  if (checked) {
+    addCheckedData(leftOrRight, item);
+  } else {
+    removeCheckedData(leftOrRight, item.id);
+  }
+};
+
+const [rightListData, addRightListData, removeRightListData] = useRightListData(
+  [],
+  checkedData
+);
+
+const { leftTitle, leftListData, transferDisabled } = useComputedData(
+  props.data,
+  targetIndex,
+  rightListData,
+  checkedData
+);
+
+const [chooseAllLeft, chooseAllRight] = useChooseAll(
+  leftListData,
+  rightListData,
+  checkedData,
+  setCheckedData
+);
+
+const chooseDisabledLeft = () => {
+  const state = leftListData.value.every((item) => item.disabled);
+  if (leftListData.length == 0 || state) {
+    return true;
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.transfer {
+  height: 300px;
+  width: 360px;
+  display: flex;
+  flex-direction: row;
+  .box {
+    width: 120px;
+    height: 100%;
+    border: 1px solid grey;
+    .list-item {
+      display: flex;
+      align-items: center;
+      //   background-color: #409eff;
+      color: #666;
+      font-size: 14px;
+      height: 30px;
+      &.disabled {
+        opacity: 0.6;
+      }
+    }
+    &.button-group {
+      border-left: 1px solid grey;
+      border-right: 1px solid grey;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      button {
+        border: none;
+        outline: none;
+        border-radius: 5px;
+        width: 38px;
+        height: 38px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #409eff;
+        color: white;
+
+        &:disabled {
+          opacity: 0.6;
+        }
+      }
+    }
+    .list-title {
+      font-size: 16px;
+      height: 38px;
+      background-color: #ddd;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #666;
+      font-weight: normal;
+      margin: 0;
+      border-bottom: 1px solid #ddd;
+    }
+  }
+}
+</style>
--- xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-ui-library/modules/diy-components/Transfer/extends/hooks.js
+++ xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-ui-library/modules/diy-components/Transfer/extends/hooks.js
@@ -1,4 +1,4 @@
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 
 export function useTargetIndex(initIndex) {
     const targetIndex = ref(initIndex);
@@ -9,7 +9,133 @@ export function useTargetIndex(initIndex) {
     return [targetIndex, setTargetIndex];
 }
 
-export function useComputedData(data, targetIndex) {
-    const leftTitle = computed(() => data[targetIndex.value].title)
-    return { leftTitle }
+export function useRightListData(initData, checkedData) {
+    const rightListData = ref(initData);
+
+    function addRightListData(newData) {
+        rightListData.value = [...rightListData.value, ...newData];
+        checkedData.left = []
+        document.getElementById('chooseAllLeft').checked = false
+
+    }
+
+    function removeRightListData(newData) {
+        const diffs = rightListData.value.filter(item => {
+            const diff = newData.find(findItem => {
+                return findItem.id == item.id
+            })
+
+            if (diff == undefined) {
+                return item
+            }
+
+        })
+
+        rightListData.value = diffs
+        checkedData.right = []
+        document.getElementById('chooseAllRight').checked = false
+    }
+    return [rightListData, addRightListData, removeRightListData];
+}
+
+
+export function useCheckedData() {
+    const checkedData = reactive({
+        left: [],
+        right: []
+    })
+
+    function addCheckedData(leftOrRight, item) {
+        switch (leftOrRight) {
+            case 'left':
+                checkedData.left.push(item)
+                    // console.log(checkedData)
+                break;
+            case 'right':
+                checkedData.right.push(item)
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    function removeCheckedData(leftOrRight, id) {
+        switch (leftOrRight) {
+            case 'left':
+                checkedData.left = checkedData.left.filter(f => f.id != id)
+                console.log(checkedData)
+                break;
+            case 'right':
+                checkedData.right = checkedData.right.filter(f => f.id != id)
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    return [removeCheckedData, addCheckedData, checkedData]
+}
+
+export function useChooseAll(leftListData, rightListData, checkedData, setCheckedData) {
+
+    const checkedLeft = ref(false)
+    const checkedRight = ref(false)
+
+    function chooseAll(chooseAllChecked, list, leftOrRight) {
+        if (chooseAllChecked) {
+            list.value.forEach((item) => {
+                if (!item.disabled) {
+                    setCheckedData("true", leftOrRight, item);
+                    document.getElementById("checkbox__" + `${item.id}`).checked = true;
+                }
+            });
+        } else {
+            checkedData.left = [];
+            list.value.forEach((item) => {
+                if (!item.disabled) {
+                    document.getElementById("checkbox__" + `${item.id}`).checked = false;
+                }
+            });
+        }
+    }
+
+    function chooseAllLeft(chooseAllChecked) {
+        chooseAll(chooseAllChecked, leftListData, 'left')
+    }
+
+    function chooseAllRight(chooseAllChecked) {
+        chooseAll(chooseAllChecked, rightListData, 'right')
+    }
+    return [chooseAllLeft, chooseAllRight]
+}
+
+export function useComputedData(allData, targetIndex, rightListData, checkedData) {
+    const leftTitle = computed(() => allData[targetIndex.value].title);
+
+    const leftListData = computed(() => {
+        const currentData = allData[targetIndex.value].data;
+
+        const diffs = currentData.filter(item => {
+            const diff = rightListData.value.find(findItem => {
+                return findItem.id == item.id
+            })
+
+            if (diff == undefined) {
+                return item
+            }
+        })
+
+        return diffs
+    });
+
+    const transferDisabled = computed(() => {
+        return {
+            left: checkedData.right.length == 0,
+            right: checkedData.left.length == 0
+        }
+    })
+
+    return { leftTitle, leftListData, transferDisabled };
 }
