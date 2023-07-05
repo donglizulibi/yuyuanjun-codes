e6a6bf05996e6223492c9d33d4edd86f461b9dcb
yuyuanjun <yuyuanjun_123@163.com>
2023-07-06 00:44:21 +0800

    x

--- xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-ui-library/src/use-components/useMagnifier.vue
+++ xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-ui-library/src/use-components/useMagnifier.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>
-    <magnifier></magnifier>
+  <div class="wrapper">
+    <magnifier
+      :imgUrl="imgUrl"
+      :blank="blank"
+      :link="link"
+      :imgWidth="imgWidth"
+      :imgHeight="imgHeight"
+      :magWidth="magWidth"
+      :magHeight="magHeight"
+    ></magnifier>
   </div>
 </template>
 
-<script setup>
+<script>
 import Magnifier from "../../modules/diy-components/Magnifier/Magnifier.vue";
+import { reactive, toRefs } from "vue";
+export default {
+  components: { Magnifier },
+  setup() {
+    const state = reactive({
+      imgUrl: "https://pix2.tvzhe.com/thumb/star/34/52/405x540.jpg",
+      blank: true,
+      link: "https://www.baidu.com",
+      imgWidth: 375,
+      imgHeight: 500,
+      magWidth: 100,
+      magHeight: 100,
+    });
+
+    return { ...toRefs(state) };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrapper {
+  width: 375px;
+  margin: 100px auto;
+}
+</style>
