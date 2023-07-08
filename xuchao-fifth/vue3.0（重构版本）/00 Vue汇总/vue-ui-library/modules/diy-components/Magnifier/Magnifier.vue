7b8293a6b40add5543379b983094bc1312a0409a
yuyuanjun <yuyuanjun_123@163.com>
2023-07-08 23:46:53 +0800

    x

--- xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-ui-library/modules/diy-components/Magnifier/Magnifier.vue
+++ xuchao-fifth/vue3.0（重构版本）/00 Vue汇总/vue-ui-library/modules/diy-components/Magnifier/Magnifier.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>zxcv</div>
+  <div
+    class="img-warp"
+    :style="{
+      width: imgWidth + 'px',
+      height: imgHeight + 'px',
+    }"
+  >
+    <a class="mag-warp" :href="link" :target="blank ? '_blank' : ''"></a>
+  </div>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  name: "Magnifier",
+  props: {
+    // imgUrl: "https://pix2.tvzhe.com/thumb/star/34/52/405x540.jpg",
+    // blank: true,
+    // link: "https://www.baidu.com",
+    // imgWidth: 375,
+    // imgHeight: 500,
+    // magWidth: 100,
+    // magHeight: 100,
+  },
+};
+</script>
 
 <style lang="scss" scoped></style>
