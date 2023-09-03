c70ae987ec5dbc97d3bd800aabcde6be4add17c7
yuyuanjun <yuyuanjun_123@163.com>
2023-09-04 05:47:02 +0800

    x

--- react综合/02 imooc react 18 with Dell/01 react快速使用及常见语法初探/02 常见语法初探/01-react-crush/src/App.jsx
+++ react综合/02 imooc react 18 with Dell/01 react快速使用及常见语法初探/02 常见语法初探/01-react-crush/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import List from "./components/List"
+import List from "./components/aList"
 function App() {
 
   return (
     <>
-        asdf
+        <h4>在react中渲染列表</h4>
         <List></List>
     </>
   )
