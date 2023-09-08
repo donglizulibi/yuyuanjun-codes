a0c5610db33adb8b0710d898b6a2dfb99eb9f11c
yuyuanjun <yuyuanjun_123@163.com>
2023-09-08 14:55:15 +0800

    x

--- react综合/02 imooc react 18 with Dell/01 react快速使用及常见语法初探/02 常见语法初探/02-react-basic/src/App.jsx
+++ react综合/02 imooc react 18 with Dell/01 react快速使用及常见语法初探/02 常见语法初探/02-react-basic/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import UseStatePrinciple from "./components/UseStatePrinciple"
+import { useState } from "react";
+import UseStatePrinciple from "./components/UseStatePrinciple";
+import StopPropagation from "./components/StopPropagation";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <> 
-    <UseStatePrinciple />
+    <>
+      <h2>关于快照态数据和batchUpdate的概念</h2>
+      <UseStatePrinciple />
+
+      <h2>冒泡与阻止冒泡</h2>
+      <StopPropagation />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react综合/02 imooc react 18 with Dell/01 react快速使用及常见语法初探/02 常见语法初探/02-react-basic/src/components/UseStatePrinciple.jsx
+++ react综合/02 imooc react 18 with Dell/01 react快速使用及常见语法初探/02 常见语法初探/02-react-basic/src/components/UseStatePrinciple.jsx
@@ -1,5 +1,37 @@
-function UseStatePrinciple(){
-    return <div>useStatePrinciple</div>
+import { useState } from "react";
+
+// 1 trigger -> render -> commit
+// 2 virtual dom: 真实dom的一个js对象表达
+// 3 快照态的数据
+// 4 batchUpdate
+
+function UseStatePrinciple() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const handleClick1 = () => {
+    // 如果只是写语句，则react则会把count当作一个快照态数据
+    // 所谓快照态数据就是使用useState初始定义时的数据
+    // 所以这三个操作中的count1都是初始定义的值
+    setCount1(count1 + 1);
+    setCount1(count1 + 1);
+    setCount1(count1 + 1);
+  };
+  const handleClick2 = () => {
+    // 如果给setCount中传入函数，则会绕过batchUpdate方法
+    // 函数中有return值，前一个return值会成为后一个函数的参数
+    setCount2((count2) => count2 + 1);
+    setCount2((count2) => count2 + 1);
+    setCount2((count2) => count2 + 1);
+  };
+  return (
+    <div>
+      <p>setCount里面直接写语句，调用三次，只增加一次</p>
+      <button onClick={handleClick1}>{count1}</button>
+      <p>setCount里面传入一个函数，调用三次，增加三次</p>
+      <button onClick={handleClick2}>{count2}</button>
+    </div>
+  );
 }
 
-export default UseStatePrinciple
+export default UseStatePrinciple;
