99b3d265360161ef7e9a9fa688ef28f330658144
yuyuanjun <yuyuanjun_123@163.com>
2023-09-18 22:00:27 +0800

    x

---
+++ react综合/02 imooc react 18 with Dell/01 react快速使用及常见语法初探/02 常见语法初探/03-react-advance/src/conponents/UseOfRef/InputComponent.jsx
@@ -0,0 +1,11 @@
+import { forwardRef } from "react";
+//
+// const InputComponent = forwardRef((props, ref) => {
+//   return <input ref={ref} type="text" />;
+// });
+
+function InputComponent(){
+    return <input type="text" />
+}
+
+export default InputComponent;
