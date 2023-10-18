04f10b1d11ab5516ef0c918fb4b4f306ada4d02f
yuyuanjun <yuyuanjun_123@163.com>
2023-10-18 12:32:17 +0800

    x

--- xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/main.js
+++ xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import installElementPlus from './plugins/element'
 import '@/styles/index.scss'
 
--- xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/plugins/element.js
+++ xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/plugins/element.js
@@ -1,7 +1,12 @@
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
+// import 'element-plus/lib/theme-chalk/index.css'
+// import locale from 'element-plus/lib/locale/lang/zh-cn'
+
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 export default (app) => {
-  app.use(ElementPlus, { locale })
+  app.use(ElementPlus, {
+    locale: zhCn
+  })
 }
