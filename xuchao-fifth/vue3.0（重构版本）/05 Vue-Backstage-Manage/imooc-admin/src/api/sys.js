b4b410bf1d4c6250bd1d370ee03281c42d5e59ef
yuyuanjun <yuyuanjun_123@163.com>
2023-10-29 11:08:55 +0800

    x

---
+++ xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/api/sys.js
@@ -0,0 +1,21 @@
+import request from '@/utils/request'
+
+/**
+ * 封装接口请求模块
+ *  return 一个promose示例
+ */
+
+/**
+ *
+ * 封装接口请求动作
+ *
+ * 希望把这个动作封装到vuex的action中，而不是点击登录之后直接调用接口
+ */
+
+export const login = (data) => {
+  return request({
+    url: '/sys/login',
+    method: 'POST'
+    // data
+  })
+}
---
+++ xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/store/modules/user.js
@@ -0,0 +1,4 @@
+export default {
+  namespaced: true
+  // 设置这个属性为true表示这是一个单独的模块，不会被合并到其他模块中
+}
