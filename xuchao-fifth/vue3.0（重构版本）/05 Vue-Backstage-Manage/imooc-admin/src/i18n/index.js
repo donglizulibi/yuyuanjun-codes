4fc13017b663ca68778a056ad1b31bd1cbb42c77
yuyuanjun <yuyuanjun_123@163.com>
2023-12-29 11:55:56 +0800

    x

---
+++ xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/i18n/index.js
@@ -0,0 +1,27 @@
+import { createI18n } from 'vue-i18n'
+const messages = {
+  en: {
+    msg: {
+      test: 'hello world'
+    }
+  },
+  zh: {
+    msg: {
+      test: '你好世界'
+    }
+  }
+}
+
+const locale = 'zh'
+
+const i18n = createI18n({
+  // 使用composition api
+  legacy: false,
+
+  // 全局使用 t 函数
+  globalInjection: true,
+  locale,
+  messages
+})
+
+export default i18n
--- xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/main.js
+++ xuchao-fifth/vue3.0（重构版本）/05 Vue-Backstage-Manage/imooc-admin/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './i18n'
 
 import installElementPlus from './plugins/element'
 import '@/styles/index.scss'
@@ -15,7 +16,8 @@ import installIcons from '@/icons'
 // 导入路由鉴权模块
 import '@/permission.js'
 
+console.log('i18n: ', i18n)
 const app = createApp(App)
 installElementPlus(app)
 installIcons(app)
-app.use(store).use(router).mount('#app')
+app.use(i18n).use(store).use(router).mount('#app')
